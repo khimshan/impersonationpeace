68a935c6d425de97075569a9f87db4ee5840c370
Shan Yap <KYap@BitTitan.com>
2018-10-23 20:41:00 +0800

    Remove background GLOBALTABMAP

--- background.js
+++ background.js
@@ -1,71 +1,124 @@
 //var checkPendingImpersonationTab = setInterval(changePendingImpersonationTab, 5000); //needs to use timer to wait for all AJAX operations to complete
-function onError(error) {
+function onError(error)
+{
     console.log(`Error: ${error}`);
 }
 
-function onMWTabCreated(tab) {
+function onMWTabCreated(tab)
+{
     const TIMERTIMES = 10; // set timer to monitor tab for 10 seconds
     var intervalTimes = 0;
 
     var checkPendingImpersonationTabTimer = setInterval(changePendingImpersonationTab, 1000); //start 1s interval check for tab URL
 
-    function changePendingImpersonationTab() {
+    function changePendingImpersonationTab()
+    {
         var gettingInfo = browser.tabs.get(tab.id); // needs to re-get tab to refresh the latest tab URL
         gettingInfo.then(onGot, onError);
-        
-        if (intervalTimes > TIMERTIMES) {
+
+        if (intervalTimes > TIMERTIMES)
+        {
             clearInterval(checkPendingImpersonationTabTimer); // disable polling timer after allowed time limit
         }
         console.log("Timer is still running !!!");
         intervalTimes++;
     }
 
-    function onGot(tab) {
+    function onGot(tab)
+    {
         console.log("Inside is bolIsManageURL !!! URL : " + tab.url);
-        if (tab.url.indexOf("manage.bittitan.com") != -1) { //tab which has just logged in through IMPERSONATION
+        if (tab.url.indexOf("manage.bittitan.com") != -1)
+        { //tab which has just logged in through IMPERSONATION
             console.log("Inside is bolIsManageURL : TRUE !");
             browser.tabs.update(tab.id, { url: "https://migrationwiz.bittitan.com/app/" });
             clearInterval(checkPendingImpersonationTabTimer); // disable polling timer URL changed
-        } else {
-            console.log("Inside is bolIsManageURL : FFFFF !"  + tab.url);
+        } else
+        {
+            console.log("Inside is bolIsManageURL : FFFFF !" + tab.url);
         };
     }
 }
 
-function onDPTabCreated(tab) {
+function onDPTabCreated(tab)
+{
     const TIMERTIMES = 10; // set timer to monitor tab for 10 seconds
     var intervalTimes = 0;
 
     var checkPendingImpersonationTabTimer = setInterval(changePendingImpersonationTab, 1000); //start 1s interval check for tab URL
 
-    function changePendingImpersonationTab() {
+    function changePendingImpersonationTab()
+    {
         var gettingInfo = browser.tabs.get(tab.id); // needs to re-get tab to refresh the latest tab URL
         gettingInfo.then(onGot, onError);
-        
-        if (intervalTimes > TIMERTIMES) {
+
+        if (intervalTimes > TIMERTIMES)
+        {
             clearInterval(checkPendingImpersonationTabTimer); // disable polling timer after allowed time limit
         }
         intervalTimes++;
     }
 
-    function onGot(tab) {
-        if (tab.url.indexOf("manage.bittitan.com") != -1) { //tab which has just logged in through IMPERSONATION
+    function onGot(tab)
+    {
+        if (tab.url.indexOf("manage.bittitan.com") != -1)
+        { //tab which has just logged in through IMPERSONATION
             browser.tabs.update(tab.id, { url: "https://manage.bittitan.com/customers" });
-            browser.tabs.create({ "url": "https://manage.bittitan.com/device-management/deploymentpro", "active" : false });
+            browser.tabs.create({ "url": "https://manage.bittitan.com/device-management/deploymentpro", "active": false });
             clearInterval(checkPendingImpersonationTabTimer); // disable polling timer URL changed
         }
     }
 }
 
-browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    console.log("BACKGROUNG EVENT LISTENER FUNCTION RECEIVEED !!!!!!!");
-    if (request.action === 'open_mw_tab') {
-        var creating = browser.tabs.create({ "url": request.tabURL, "active" : false });
+function onDirectTabCreated(tab, targetURL)
+{
+    const TIMERTIMES = 10; // set timer to monitor tab for 10 seconds
+    var intervalTimes = 0;
+
+    var checkPendingImpersonationTabTimer = setInterval(changePendingImpersonationTab, 1000); //start 1s interval check for tab URL
+
+    function changePendingImpersonationTab()
+    {
+        var gettingInfo = browser.tabs.get(tab.id); // needs to re-get tab to refresh the latest tab URL
+        gettingInfo.then(onGot, onError);
+
+        if (intervalTimes > TIMERTIMES)
+        {
+            clearInterval(checkPendingImpersonationTabTimer); // disable polling timer after allowed time limit
+        }
+        intervalTimes++;
+    }
+
+    function onGot(tab)
+    {
+        if (tab.url.indexOf("manage.bittitan.com") != -1)
+        { //tab which has just logged in through IMPERSONATION
+            console.log("before redirect : " + targetURL);
+            browser.tabs.update(tab.id, { url: targetURL });
+            clearInterval(checkPendingImpersonationTabTimer); // disable polling timer URL changed
+        }
+    }
+}
+
+
+browser.runtime.onMessage.addListener(function (request, sender, sendResponse)
+{
+    console.log("top of OnMessage : " + request.targetURL);
+    if (request.action === 'open_mw_tab')
+    {
+        var creating = browser.tabs.create({ "url": request.impersonationURL, "active": false });
         creating.then(onMWTabCreated, onError);
 
     } else if (request.action === 'open_dp_tab') 
     {
-        var creating = browser.tabs.create({ "url": request.tabURL, "active" : false });
+        var creating = browser.tabs.create({ "url": request.impersonationURL, "active": false });
         creating.then(onDPTabCreated, onError);
+    } else if (request.action === 'open_direct_tab') 
+    {
+        var creating = browser.tabs.create({ "url": request.impersonationURL, "active": false });
+        creating.then(function (tab)
+        {
+            console.log("top of open direct tab : " + request.targetURL);
+            onDirectTabCreated(tab, request.targetURL);
+        }, onError);
     }
 });
--- content.js
+++ content.js
@@ -1,32 +1,47 @@
 var jsInitChecktimer = setInterval(checkForJS_Finish, 2000); //needs to use timer to wait as AJAX operations does not refresh URL and hence unable to rely on change in URL to trigger changes
+var GLOBALMAP = new Map();
 
-function checkForJS_Finish() {
+function checkForJS_Finish()
+{
 
     var anchorTags, visitorEmailElem;
 
-    if (document.getElementsByClassName("zd-comment").length > 0) {
+    if (document.getElementsByClassName("zd-comment").length > 0)
+    {
         //clearInterval(jsInitChecktimer); //
 
         anchorTags = document.getElementsByTagName('a'); // extracts all ANCHOR tag in document
-        for (var k = 0; k < anchorTags.length; k++) {
-            if (anchorTags[k].href.indexOf('mailto') !== -1) {
+        for (var k = 0; k < anchorTags.length; k++)
+        {
+            if (anchorTags[k].href.indexOf('mailto') !== -1)
+            {
                 var fields = anchorTags[k].href.split(':');
                 anchorTags[k].href = "https://internal.bittitan.com/Impersonate/" + fields[1];
                 anchorTags[k].addEventListener('click', sendClickEventToBackground);
-            } else if (anchorTags[k].innerHTML.indexOf('Account Name') != -1) {
-                console.info("Reference : " + anchorTags[k].href);
+            } else if (anchorTags[k].href.indexOf('migrationwiz.bittitan.com') !== -1)
+            {
+                anchorTags[k].addEventListener('click', sendClickEventToBackground);
             }
         }
         var regextSearchAccountBlock = /<strong>Account Name[\s\S]+?<p dir="auto">/gi;
         var regexSearchImpersonationEmail = /<strong>Account Name.*href=\"https:.*\/Impersonate\/(\S+)\"/i;
         var foundAccountBlock = new XMLSerializer().serializeToString(document).match(regextSearchAccountBlock);
-        foundAccountBlock.forEach(function(s) {console.log("+++++++++++++++++++++++++++++++++++++\n" + s)});
+
+        if (foundAccountBlock != null)
+        {
+            foundAccountBlock.forEach(extractImpersonationIDandLinkstoMap);
+        }
+        console.log("++++++++GLOBALMAP+++++++++++");
+        console.dir(GLOBALMAP);
         //console.log(new XMLSerializer().serializeToString(document));
-    } else { // check for chat window popup
+    } else
+    { // check for chat window popup
         visitorEmailElem = document.getElementsByClassName('meshim_dashboard_components_chatPanel_VisitorInfoTextField visitor_email info')[0];
 
-        if (visitorEmailElem !== null && (typeof visitorEmailElem !== 'undefined')) {
-            if (document.getElementsByClassName('impersonation_link_class')[0] == null) {
+        if (visitorEmailElem !== null && (typeof visitorEmailElem !== 'undefined'))
+        {
+            if (document.getElementsByClassName('impersonation_link_class')[0] == null)
+            {
                 var link = document.createElement('a');
                 link.className = 'impersonation_link_class';
                 link.setAttribute('href', 'https://internal.bittitan.com/Impersonate/' + visitorEmailElem.value);
@@ -35,26 +50,67 @@ function checkForJS_Finish() {
                 link.addEventListener('click', sendClickEventToBackground);
                 link.innerHTML = "Impersonate";
                 visitorEmailElem.parentElement.appendChild(link);
-            } else {
+            } else
+            {
                 document.getElementsByClassName('impersonation_link_class')[0].setAttribute('href', 'https://internal.bittitan.com/Impersonate/' + visitorEmailElem.value);
                 document.getElementsByClassName('impersonation_link_class')[0].addEventListener('click', sendClickEventToBackground);
             }
-        } else { //clears old impersonation information
-            if (document.getElementsByClassName('impersonation_link_class')[0] !== null && (typeof document.getElementsByClassName('impersonation_link_class')[0] !== 'undefined')) {
+        } else
+        { //clears old impersonation information
+            if (document.getElementsByClassName('impersonation_link_class')[0] !== null && (typeof document.getElementsByClassName('impersonation_link_class')[0] !== 'undefined'))
+            {
                 document.getElementsByClassName('impersonation_link_class')[0].setAttribute('href', '');
             }
         }
     }
 }
 
-function sendClickEventToBackground(e) {
-    if (e.ctrlKey) {
+function extractImpersonationIDandLinkstoMap(accountBlock)
+{
+    var regexSearchHREF = /<a href=\"(\S+)\"/gi;
+
+    var foundHREF = regexSearchHREF.exec(accountBlock);
+    var count = 1;
+    var impersonationIDForBlock = "";
+
+    console.log("+++++++++++++++++++++++++++++++++++++\n");
+    while (foundHREF != null)
+    {
+        if (count === 1)
+        {
+            //impersonationIDForBlock = foundHREF[1].split(`https://internal.bittitan.com/Impersonate/`)[1];
+            impersonationIDForBlock = foundHREF[1];
+            count++;
+        } else
+        {
+            GLOBALMAP.set(foundHREF[1], impersonationIDForBlock);
+        }
+        console.log(impersonationIDForBlock);
+        console.dir(foundHREF);
+        foundHREF = regexSearchHREF.exec(accountBlock);
+    }
+}
+
+function sendClickEventToBackground(e)
+{
+    console.log("---------- Detected Click Event ----------------");
+    if (e.ctrlKey)
+    {
         e.stopPropagation();
         e.preventDefault();
-        browser.runtime.sendMessage({ action: 'open_mw_tab', tabURL: e.target.href });
-    } else if (e.altKey) {
+        if (e.target.href.indexOf('migrationwiz.bittitan.com') == -1)
+        {
+            browser.runtime.sendMessage({ action: 'open_mw_tab', impersonationURL: e.target.href });
+        } else
+        {
+            impersonationHREF = GLOBALMAP.get(e.target.href);
+            browser.runtime.sendMessage({ action: 'open_direct_tab', impersonationURL: impersonationHREF, targetURL: e.target.href });
+        }
+    } else if (e.altKey)
+    {
+        console.log("---------- Detected ALT KEY pressed ----------------");
         e.stopPropagation();
         e.preventDefault();
-        browser.runtime.sendMessage({ action: 'open_dp_tab', tabURL: e.target.href });
+        browser.runtime.sendMessage({ action: 'open_dp_tab', impersonationURL: e.target.href });
     }
 }
